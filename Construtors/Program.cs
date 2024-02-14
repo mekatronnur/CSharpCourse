a52d4fafc74f8b1487ad108f24c7d639bb457a56
Nursel DÜZDAR <nursel.duzdar@istikbal.com.tr>
2024-02-14 16:48:43 +0300

    updated

---
+++ Construtors/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Customer Customers = new Customer();
+Customer customer1 = new Customer(2, "Nursel", "Düzdar", "Kayseri");
+
+Customer customer2 = new Customer
+{
+    Id = 1,
+    FirstName = "mERVE",
+    LastName = "dENİZ",
+    City = "kAYSERİ"
+};
+class Customer
+{
+    //Default Constructor
+    public Customer()
+    {
+
+    }
+
+
+    public Customer(int id, string _FirstName, string _LastName, string _city)
+    {
+        Id = id;
+        FirstName = _FirstName;
+        LastName = _LastName;
+        City = _city;
+    }
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+
+}
---
+++ ReferanceTypes/Program.cs
@@ -0,0 +1,43 @@
+﻿https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int sayi1 = 10;
+int sayi2 = 20;
+sayi2 = 100;
+sayi1 = sayi2;
+
+Console.WriteLine("Sayi1:" + sayi1);
+
+//int decimal float enum boolen:value types
+
+int[] numbers = new int[] { 1, 3, 4, 5 };
+int[] numbers2 = new int[] { 10, 45, 34 };
+numbers = numbers2;
+numbers2[0] = 43;
+
+Console.WriteLine("Sayılar[0]:" + numbers[0]);
+
+Person person = new Person();
+Person person1 = new Person();
+person.Name = "nuR";
+person1 = person;
+
+person.Name = "nURSEL";
+Console.WriteLine(person1.Name);
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string  LastName { get; set; }
+}
+
+class Customer:Person
+{
+    public string CreditCardNumber {  get; set; }   
+}
+
+class Employee:Person
+{
+    public int EmployeeNumber { get; set; }
+}
