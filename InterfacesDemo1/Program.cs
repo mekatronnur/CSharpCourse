15c8f9a7d79198afc36832495df184bcfca9edcb
Nursel DÜZDAR <nursel.duzdar@istikbal.com.tr>
2024-02-14 17:50:35 +0300

    Added

---
+++ InterfacesDemo1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+IPersonManager personManager=new CustomerManager();
+ProjectManager projectManager=new ProjectManager();
+projectManager.Add(personManager);
+personManager.Add();
+projectManager.Add(new CustomerManager());
+ public interface  IPersonManager
+{
+    void Add();
+    
+}
+
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Added");
+    }
+}
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        throw new NotImplementedException();
+    }
+}
+//Interface new lenemez
+
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+      personManager.Add();
+    }
+}
